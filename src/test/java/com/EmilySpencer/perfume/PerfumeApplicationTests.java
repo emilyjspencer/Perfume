9d868864c55b6b6b4ff649e71084d1aa496cd19e
emilyjspencer <emilyspencer89@hotmail.co.uk>
2021-04-01 17:41:54 +0100

    Adds initial commit

---
+++ src/test/java/com/EmilySpencer/perfume/PerfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmilySpencer.perfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
