719eb46ad1a83a8645d7d51dafc63f4e8eb8b1be
emilyjspencer <emilyspencer89@hotmail.co.uk>
2021-04-01 23:06:22 +0100

    Adds images

--- src/main/java/com/EmilySpencer/perfume/controller/BrandController.java
+++ src/main/java/com/EmilySpencer/perfume/controller/BrandController.java
@@ -4,8 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -42,5 +45,26 @@ public class BrandController {
 		model.addAttribute("message", "Brand" + brand.getName() + "has been added");
 		return new ModelAndView("WEB-INF/allBrands.jsp", "allBrands", brandService.retrieveAll());
 	}
+	
+	@GetMapping("/EditBrand/{brandId}")
+	public ModelAndView editBrand(@PathVariable("brandId") Long brandId, Model model) {
+		return new ModelAndView("../WEB-INF/editBrand.jsp", "brand", brandService.findABrand(brandId));
+	}
 
+	@PostMapping("/EditBrandSubmit")
+	public ModelAndView editBrandSubmit(Brand brand, ModelMap model) {
+		brandService.update(brand);
+		return new ModelAndView("forward://WEB-INF/allBrands.jsp", "allBrands", brandService.retrieveAll());
+	}
+
+	@PostMapping(value = "/EditBrandSubmit", params = { "Delete=Delete" })
+	public ModelAndView editBrandSubmit(Brand brand, BindingResult bindingResult, ModelMap model, Long brandId) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("message", "this brand still has perfumes in the stores");
+			return new ModelAndView("WEB-INF/editBrand.jsp");
+		}
+		brandService.delete(brandId);
+		model.addAttribute("message", "the brand was deleted");
+		return new ModelAndView("forward://WEB-INF/allBrands.jsp", "allBrands", brandService.retrieveAll());
+	}
 }
